79036870a69513c632ec4d68bbf6d4a32f71dcc7
broccoli <aq0011@naver.com>
2023-11-06 22:13:10 +0900

    RandomMapGenerator
    맵을 넣은 숫자 크기에 따라 a*b 형식의 맵을 생성하는 스크립트 작성

--- Assets/Map/DungeonGenerator.cs
+++ Assets/Map/DungeonGenerator.cs
@@ -1,12 +1,14 @@
 using System.Collections;
 using System.Collections.Generic;
+using UnityEditor.Profiling.Memory.Experimental;
 using UnityEngine;
 
 public class DungeonGenerator : MonoBehaviour
 {
-    [Header("맵의 전체 크기를 정하는 헤더")]
-    public float Max_x = 500f;
-    public float Max_y = 500f;
+    [Header("맵의 전체 크기를 정하는 헤더")] //4*4
+    public int Max_x = 4;
+    public int Max_y = 4;
+    public float length = 10f;
 
     [Header("맵들의 프리팹 저장")]
     public GameObject[] maps = new GameObject[5];
@@ -17,13 +19,15 @@ public class DungeonGenerator : MonoBehaviour
         MapGenerator(Max_x, Max_y);
     }
 
-    private void Update()
+    void MapGenerator(int a, int b)
     {
-        
-    }
-
-    void MapGenerator(float a, float b) 
-    {
-
+        for(int i = 0; i< a; i++)
+        {
+            for(int j = 0; j < b; j++)
+            {
+                int rand_num = Random.Range(0, maps.Length);
+                Instantiate(maps[rand_num], new Vector3(i*length, 16.5f, j*length), Quaternion.identity);
+            }
+        }
     }
 }
